680ea4b7fb3792bee7b7012b48d27612e64b85fe
Anza Aleem <106549351+anzaaleem@users.noreply.github.com>
2023-08-03 22:10:46 +0500

    Update weather_scraper.php

--- weather_scraper.php
+++ weather_scraper.php
@@ -1,76 +1,150 @@
 <?php
+    
+    $weather = "";
+    $error = "";
+    
+    if (array_key_exists('city', $_GET)) {
+        
+        $city = str_replace(' ', '', $_GET['city']);
+        
+        $file_headers = @get_headers("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
+        
+        
+        if($file_headers[0] == 'HTTP/1.1 404 Not Found') {
+    
+            $error = "That city could not be found.";
 
-$error="";
-$weather="";
+        } else {
+        
+        $forecastPage = file_get_contents("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
+        
+        $pageArray = explode('3 Day Weather Forecast Summary:</b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">', $forecastPage);
+            
+        if (sizeof($pageArray) > 1) {
+        
+                $secondPageArray = explode('</span></span></span>', $pageArray[1]);
+            
+                if (sizeof($secondPageArray) > 1) {
 
-if(array_key_exists('city',$_GET)){
-$city=str_replace(' ','',$_GET['city']);
+                    $weather = $secondPageArray[0];
+                    
+                } else {
+                    
+                    $error = "That city could not be found.";
+                    
+                }
+            
+            } else {
+            
+                $error = "That city could not be found.";
+            
+            }
+        
+        }
+        
+    }
 
-$file_headers = @get_headers("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
-if($file_headers[0] == 'HTTP/1.1 404 Not Found') {
-    $error="That city could not be found";
-}
-else{
 
-$foreCast= file_get_contents("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
-$pageArray= explode('3 Day Weather Forecast Summary:</b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">',$foreCast);
-
-
-if(sizeof($pageArray)>1)
-{
-$secondPageArray= explode('</span></span></span>',$pageArray[1]);
-if(sizeof($secondPageArray)>1){
-$weather=$secondPageArray[0];
-}else{
-    $error="That city could not be found";
-}
-}else{
-    $error="That city could not be found";
-}
-}
-}
 ?>
 
-<!doctype html>
+
+<!DOCTYPE html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
+    <!-- Required meta tags always come first -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
 
+      <title>Weather Scraper</title>
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Weather App</title>
-    <link href="weather_scraper.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+      
+      <style type="text/css">
+      
+      html { 
+          background: url(background.jpeg) no-repeat center center fixed; 
+          -webkit-background-size: cover;
+          -moz-background-size: cover;
+          -o-background-size: cover;
+          background-size: cover;
+          }
+        
+          body {
+              
+              background: none;
+              
+          }
+          
+          .container {
+              
+              text-align: center;
+              margin-top: 100px;
+              width: 450px;
+              
+          }
+          
+          input {
+              
+              margin: 20px 0;
+              
+          }
+          
+          #weather {
+              
+              margin-top:15px;
+              
+          }
+         
+      </style>
+      
   </head>
   <body>
-    <div class="container">
-       <h1>What's the Weather?</h1>
-       <form  action="#" method="get">
-           <fieldset class="form-group">
+    
+      <div class="container">
+      
+          <h1>What's The Weather?</h1>
+          
+          
+          
+          <form>
+  <fieldset class="form-group">
+    <label for="city">Enter the name of a city.</label>
+    <input type="text" class="form-control" name="city" id="city" placeholder="Eg. London, Tokyo" value = "<?php 
+																										   
+																										   if (array_key_exists('city', $_GET)) {
+																										   
+																										   echo $_GET['city']; 
+																										   
+																										   }
+																										   
+																										   ?>">
+  </fieldset>
+  
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+      
+          <div id="weather"><?php 
+              
+              if ($weather) {
+                  
+                  echo '<div class="alert alert-success" role="alert">
+  '.$weather.'
+</div>';
+                  
+              } else if ($error) {
+                  
+                  echo '<div class="alert alert-danger" role="alert">
+  '.$error.'
+</div>';
+                  
+              }
+              
+              ?></div>
+      </div>
 
-           <label>Enter Your City Name</label>
-           <input class="form-control" type="text" name="city" placeholder="e.g Pakistan, India" value="<?php echo $_GET['city']; ?>">
-           </fieldset>
-
-           <button type="submit" class="btn btn-primary">Submit</button>
-       </form>
-
-       <div id="weather">
-        <?php
-        if($weather){
-
-echo '<div class="alert alert-success" role="alert">'.$weather.'</div>';
-        }
-       
-        else($error){
-            echo  '<div class="alert alert-danger" role="alert">'.$error.'</div>';
-        } 
-        ?>
-       </div>
-</div>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <!-- jQuery first, then Bootstrap JS. -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
   </body>
 </html>
